a4bdbdc04db959291c0f627383d96ed82f625da3
Kevin Waterson <kevin.waterson@gmail.com>
2014-04-25 22:41:25 +1000

    Added templates directory

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ flamingo
 ========
 
 Flamingo - A Go/GoLang Shopping Cart, Warehousing and Shipping Management System
+
+The Flamingo system comes in two parts. The web component, and the API component which the web component connects to
--- web/main.go
+++ web/main.go
@@ -1,9 +1,26 @@
 package main
 
-import(
-	"fmt"
+import (
+  "log"
+  "net/http"
+  "time"
 )
 
-func main(){
-	fmt.Println("Flamingo!")
+func logHandler(next http.Handler) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%v: %v", r.Method, r.URL.Path)
+    next.ServeHTTP(w, r)
+  }
+}
+
+func main() {
+  http.Handle("/est", logHandler(http.HandlerFunc(est)))
+
+  log.Println("Listening...")
+  http.ListenAndServe(":3000", nil)
+}
+
+func est(w http.ResponseWriter, r *http.Request) {
+  tm := time.Now().In(time.FixedZone("EST", 0)).Format(time.RFC1123)
+  w.Write([]byte("The time is: " + tm))
 }
