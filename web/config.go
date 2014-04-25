de96994f8716ad74213356775509a831f1ae8e2c
Kevin Waterson <kevin.waterson@gmail.com>
2014-04-26 00:12:29 +1000

    Added config file

---
+++ web/config.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+        HTTP_PORT = "3000"
+        API_KEY = "36b10d1c319Y7b450BE5795f0e5a30c252aB856a450bE5795f0e"
+        SERVER_PROTOCOL = "https://"
+
+	TITLE = "Flamingo"
+)
+
